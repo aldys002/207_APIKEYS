3d862f734aceba57e7a1fddaa8cc78c25667da79
aldys002 <igidiaaldys@gmail.com>
2025-11-21 13:48:11 +0700

    database apikey

---
+++ backend/models/ApiKey.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+const db = require("../config/database");
+
+const ApiKey = db.define("ApiKey", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    key: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    out_date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    tableName: "api_keys",
+    timestamps: false // supaya kita pakai created_at / updated_at manual
+});
+
+module.exports = ApiKey;
